0892b458b67e33fb6eac125b8735689d51876c37
Kieran Chaase Morford <kieranmorford@gmail.com>
2025-10-22 16:13:16 -0600

    Finished Custom Register Tests

--- server/src/test/java/Service/UserServiceTest.java
+++ server/src/test/java/Service/UserServiceTest.java
@@ -16,11 +16,26 @@ class UserServiceTest {
     void registerPositiveTest() throws AlreadyTakenException, BadRequestException {
         RegisterRequest regReq = new RegisterRequest("link","kronos","kcmorford@gmail.com");
         UserService userService = new UserService(new MemoryDataAccess());
-        assertEquals("link", userService.register(regReq).username());
+        var regRes = userService.register(regReq);
+        assertEquals("link", regRes.username());
+        assertNotNull(regRes.authToken());
     }
 
     @Test
-    void registerNegativeTest() throws AlreadyTakenException, BadRequestException {}
+    void registerRepeatTest() throws AlreadyTakenException, BadRequestException {
+        RegisterRequest regReq1 = new RegisterRequest("link","kronos","kcmorford@gmail.com");
+        RegisterRequest regReq2 = new RegisterRequest("link","zelda","kord@gmail.com");
+        UserService userService = new UserService(new MemoryDataAccess());
+        var regRes1 = userService.register(regReq1);
+        assertThrows(AlreadyTakenException.class, () -> {userService.register(regReq2);});
+    }
+
+    @Test
+    void registerBadRequestTest() throws AlreadyTakenException, BadRequestException {
+        RegisterRequest regReq = new RegisterRequest("link","kronos",null);
+        UserService userService = new UserService(new MemoryDataAccess());
+        assertThrows(BadRequestException.class, () -> {userService.register(regReq);});
+    }
 
     @Test
     void clear() {
