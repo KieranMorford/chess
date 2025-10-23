82030b05dc26d08998a4010ca6dfb1f32edb4084
Kieran Chaase Morford <kieranmorford@gmail.com>
2025-10-22 23:49:17 -0600

    Started Create Game Tests

---
+++ server/src/test/java/Service/GameServiceTest.java
@@ -0,0 +1,48 @@
+package Service;
+
+import Exceptions.AlreadyTakenException;
+import Exceptions.BadRequestException;
+import Exceptions.UnauthorizedException;
+import RequestResult.LoginRequest;
+import RequestResult.NewGameRequest;
+import RequestResult.RegisterRequest;
+import dataaccess.MemoryDataAccess;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    void newGamePositiveTest() throws BadRequestException, AlreadyTakenException, UnauthorizedException {
+        RegisterRequest regReq = new RegisterRequest("link","kronos","kcmorford@gmail.com");
+        var mDA = new MemoryDataAccess();
+        UserService userService = new UserService(mDA);
+        GameService gameService = new GameService(mDA);
+        userService.register(regReq);
+        LoginRequest logReq = new LoginRequest("link","kronos");
+        var logRes = userService.login(logReq);
+        NewGameRequest nGReq = new NewGameRequest(logRes.authToken(), "First Strand-type Game");
+        var nGRes1 = gameService.newGame(nGReq);
+        assertEquals(1,nGRes1.gameID());
+        var nGRes2 = gameService.newGame(nGReq);
+        assertEquals(2,nGRes2.gameID());
+    }
+
+    @Test
+    void newGameBadRequestTest() throws BadRequestException, AlreadyTakenException, UnauthorizedException {
+        RegisterRequest regReq = new RegisterRequest("link","kronos","kcmorford@gmail.com");
+        var mDA = new MemoryDataAccess();
+        UserService userService = new UserService(mDA);
+        GameService gameService = new GameService(mDA);
+        userService.register(regReq);
+        LoginRequest logReq = new LoginRequest("link","kronos");
+        var logRes = userService.login(logReq);
+        NewGameRequest nGReq1 = new NewGameRequest(null, "First Strand-type Game");
+        NewGameRequest nGReq2 = new NewGameRequest(logRes.authToken(), null);
+        assertThrows(BadRequestException.class, () -> {gameService.newGame(nGReq1);});
+        assertThrows(BadRequestException.class, () -> {gameService.newGame(nGReq2);});
+    }
+}
